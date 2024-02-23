b5cc90db2cf7ab7593548892df4c226899e22aab
Layomi1 <107419093+Layomi1@users.noreply.github.com>
2024-02-23 08:41:03 +0100

    creating a meme generator on a canvas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+import { ColorChromeModule } from 'ngx-color/chrome';
+import { NavbarComponent } from './navbar/navbar.component';
+import { GeneratorComponent } from './generator/generator.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [
+    RouterOutlet,
+    NavbarComponent,
+    GeneratorComponent,
+    ColorChromeModule,
+    FormsModule,
+  ],
 })
 export class AppComponent {
   title = 'angular-meme-generator';
